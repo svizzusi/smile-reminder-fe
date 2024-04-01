c169e7fc63e29aff99f42d5a3c90901a1b7fe724
Stephen Vizzusi <svizzusi13@gmail.com>
2024-04-01 00:07:49 -0700

    Finished Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 
-import NavBar from '../src/layout/navbar/Navbar'
+import Navbar from '../src/layout/navbar/Navbar'
 import Footer from '../src/layout/footer/Footer'
 import HomePage from '../src/views/HomePage'
 import DashboardPage from '../src/views/DashboardPage'
-import ExistingPatients from '../src/views/ExistingPatients'
+import ExistingPatientsPage from '../src/views/ExistingPatientsPage'
 import AboutPage from '../src/views/AboutPage'
 import PrivacyPage from '../src/views/PrivacyPage'
 import TermsPage from '../src/views/TermsPage'
@@ -15,11 +15,11 @@ function App() {
 
   return (
     <>
-    <NavBar />
+    <Navbar />
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/dashboard-page' element={<DashboardPage />} />
-      <Route path='/existing-patients' element={<ExistingPatients />} />
+      <Route path='/existing-patients-page' element={<ExistingPatientsPage />} />
       <Route path='/about-page' element={<AboutPage />} />
       <Route path='/privacy-page' element={<PrivacyPage />} />
       <Route path='/terms-page' element={<TermsPage />} />
--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,6 +1,77 @@
+import logo from '../../assets/images/logo.webp'
+import style from './Navbar.module.css'
+import { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+
 const Navbar = () => {
+  const [navExpanded, setNavExpanded] = useState(false)
+    const navigate = useNavigate()
+
+    const openNav = () => {
+      setNavExpanded(!navExpanded)
+  }
+  const closeNav = () => setNavExpanded(false)
+
+
   return (
-    <></>
+    <header>
+      <nav className={style.nav}>
+        <Link to={'/'}>
+          <img 
+            className={style.logo} 
+            src={logo} 
+            alt={logo} 
+          />
+        </Link>
+        <ul className={navExpanded ? `${style.navLinks} ${style.expanded}` : style.navLinks}>
+          <li>
+            <span
+              onClick={() => { 
+                  // setShowLogin(true), 
+                  // setShowSignup(false), 
+                  closeNav() 
+                }}
+            >Login</span>
+          </li>
+          <li>
+            <span
+              onClick={() => { 
+                // setShowSignup(true), 
+                // setShowLogin(false), 
+                closeNav() 
+              }}
+            >SignUp</span>
+          </li>
+          <li>
+            <span
+              onClick={() => { 
+                closeNav(),
+                navigate('/') 
+            }}
+            >Home</span>
+          </li>
+          <li>
+            <span
+              onClick={() => { 
+                // setShowSignup(false), 
+                // setShowLogin(false),
+                // setUser(false),
+                // window.sessionStorage.removeItem('userName');, // Clear user name
+                // window.sessionStorage.removeItem('userId');, // Clear user ID
+                closeNav(),
+                navigate('/')
+            }}
+            >Logout</span>
+          </li>
+        </ul>
+        <div 
+            className={navExpanded ? `${style['icon-three']} ${style['active-three']}` : style['icon-three']}
+            onClick={openNav}
+        >
+            <div className={`${style['hamburger']} ${style['hamburger-three']}`}></div>
+        </div>
+      </nav>
+    </header>
   )
 };
 
