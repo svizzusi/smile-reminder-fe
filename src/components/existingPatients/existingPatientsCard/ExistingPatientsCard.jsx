fba6146b4d0becd5044a9cbb54a5c140c7d7e022
Stephen Vizzusi <svizzusi13@gmail.com>
2024-04-02 23:12:39 -0700

    Edited existing patients card

--- src/components/existingPatients/existingPatientsCard/ExistingPatientsCard.jsx
+++ src/components/existingPatients/existingPatientsCard/ExistingPatientsCard.jsx
@@ -1,6 +1,44 @@
+import style from './ExistingPatientsCard.module.css'
+import {BsTrash} from 'react-icons/bs'
+import {AiOutlineEdit, AiOutlineShoppingCart} from 'react-icons/ai'
+// import axios from 'axios';
+
 const existingPatientsCard = () => {
   return (
-    <></>
+    <>
+      <tbody className={style.existingPatientsTableBody}>
+        <tr 
+          className={style.existingPatientsTableRowCard} 
+          >
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>{}</td>
+          <td>
+            <span className={style.existingPatientsEditBtn}
+              >
+                <AiOutlineEdit
+                // onClick={() => {
+                //   setShowEditProduct(true)
+                //   setProductId(product._id)
+                // }}
+              />
+            </span>
+          </td>
+          <td>
+            <span 
+              // onClick={() => handleDelete(product._id)}
+              className={style.existingPatientsDeleteBtn}
+              >
+                <BsTrash />
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <td>No existing patients found</td>
+        </tr>
+      </tbody>
+    </>
   )
 };
 
--- src/components/existingPatients/existingPatientsHeader/ExistingPatientsHeader.jsx
+++ src/components/existingPatients/existingPatientsHeader/ExistingPatientsHeader.jsx
@@ -1,6 +1,17 @@
+import style from './ExistingPatientsHeader.module.css'
+
 const ExistingPatientsHeader = () => {
   return (
-    <></>
+    <thead className={style.existingPatientsTableHeader}>
+      <tr className={style.existingPatientsTableRowHeader}>
+        <th>Last Name</th>
+        <th>First Name</th>
+        <th>Phone Number</th>
+        <th>Email</th>
+        <th>Edit Patient</th>
+        <th>Delete Patient</th>
+      </tr>
+    </thead>
   )
 };
 
